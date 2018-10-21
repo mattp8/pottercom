8e1a9803cb7cd29525c7c7687ccf049abff057f3
matt <matt.pate@finimize.com>
2018-10-21 18:09:15 +0100

    add script tags around potterconfig

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ class IndexPage extends Component {
 
   render() {
     const { color, buttonText } = this.state;
-    const code = `var potterconfig = { color:${color};}
+    const code = `<script>var potterconfig = { color:${color};}</script>
     <script src="https://cdn.jsdelivr.net/gh/mattp8/pottercom.js@1.0/src/index.js" />`;
 
     return (
