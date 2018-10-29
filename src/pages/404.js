4e3b9a0a784e91fca2508ae1398eae628c1c9d54
matt <matt.pate@finimize.com>
2018-10-29 22:45:49 +0000

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Box, Heading, Text } from 'rebass';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+import { Flex, Text } from 'rebass';
 import Layout from '../components/Layout';
+
+import Sign from '../assets/images/sign.svg';
 
 const NotFoundPage = () => (
   <Layout>
-    <Box>
+    <Flex justifyContent="center" flexDirection="column">
       <Helmet>
-        <title>Page not found</title>
+        <title>Sorry wrong station | Page not found</title>
       </Helmet>
 
-      <Heading my={3}>Page not found</Heading>
-      <Text is="p" my={3}>
-        The requested page is unavailable.
+      <Head my={3}>Sorry the train does not stop at this platform</Head>
+      <Text color="#f7f7f7" textAlign="center" mt={3}>
+        Please exit at the next station <StyledLink to="/">here</StyledLink>
       </Text>
-    </Box>
+      <SignImg src={Sign} alt="page not found" />
+    </Flex>
   </Layout>
 );
 
 export default NotFoundPage;
+
+const Head = styled.h1`
+  color: #fff;
+  max-width: 450px;
+  margin: 0 auto;
+  font-size: 30px;
+  text-align: center;
+  margin-top: 40px;
+`;
+
+const SignImg = styled.img`
+  width: 200px;
+  height: auto;
+  max-width: 80%;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 40px;
+`;
+
+const StyledLink = styled(Link)`
+  color: #00b2ff;
+  text-decoration: none;
+  font-weight: 600;
+  text-align: center;
+  transition: 200ms ease;
+  padding-top: 16px;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`;
