80d8082ccb36a3cbbf7816b2245e3310efd9cf1b
matt <matt.pate@finimize.com>
2018-10-29 23:38:43 +0000

    make colour insscript dyanmic

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ class IndexPage extends Component {
 
   render() {
     const { color, buttonText } = this.state;
-    const code = `<script>var potterconfig = { color:"#000000"}; </script>
+    const code = `<script>var potterconfig = { color:"${color}"}; </script>
     <script src="https://cdn.jsdelivr.net/gh/mattp8/pottercom.js@1.3/src/index.js"></script>`;
 
     return (
